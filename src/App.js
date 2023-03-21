e999172a21c4dbc83ca7ed848a7f0494d446a3d8
amassock <ajmassock@gmail.com>
2023-03-20 23:45:45 -0500

    move src out of lib directory. added public index.html to display app. wrote first test for insert form. resolved first test

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import InsertForm from "./components/InsertForm.js";
+
+function App() {
+  return (
+    <div className="App">
+      <InsertForm />
+    </div>
+  );
+}
+
+export default App;
