458f70ca5518a7c7e90a562d4e761bbd86b5cc65
amassock <ajmassock@gmail.com>
2023-03-21 20:55:51 -0500

    add test for each part of the form. update component to pass tests

--- src/components/InsertForm.js
+++ src/components/InsertForm.js
@@ -4,6 +4,13 @@ export default function InsertForm() {
   return (
     <div className="insert-form-container">
       <h1>Add Product</h1>
+      <input id="title" placeholder="Product" />
+      <br />
+      <input id="desc" placeholder="Description" />
+      <br />
+      <input id="price" placeholder="Price" />
+      <br />
+      <p id="date">Date Posted</p>
     </div>
   );
 }
--- src/components/InsertForm.test.js
+++ src/components/InsertForm.test.js
@@ -5,11 +5,31 @@ import InsertForm from "./InsertForm";
 
 // We will describe a block of tests
 describe("Form component to insert product", () => {
-  // we will write one individual test
+  const component = shallow(<InsertForm />);
+
   it('should have a header that says "Add Product"', () => {
-    // Shallow rendering renders a component without rendering any of its children
-    const component = shallow(<InsertForm />);
-    // We create an assertion within the test that checks if our component renders our name prop
     expect(component.contains(<h1>Add Product</h1>)).toBe(true);
+  });
+
+  it('should contain an input field that has a "Product" placeholder', () => {
+    expect(component.contains(<input id="title" placeholder="Product" />)).toBe(
+      true
+    );
+  });
+
+  it('should have a input with placeholder "Description"', () => {
+    expect(
+      component.contains(<input id="desc" placeholder="Description" />)
+    ).toBe(true);
+  });
+
+  it('should have a third input with a "Price" placeholder', () => {
+    expect(component.contains(<input id="price" placeholder="Price" />)).toBe(
+      true
+    );
+  });
+
+  it("should have a p tag with the date posted", () => {
+    expect(component.contains(<p id="date">Date Posted</p>)).toBe(true);
   });
 });
