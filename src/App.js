16537cf9aa532b7083f4f85c23e10fd5e0db5b01
amassock <ajmassock@gmail.com>
2023-03-27 19:17:55 -0500

    integrate api call to get all products from backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import HomePage from "./components/HomePage.js";
 import InsertForm from "./components/InsertForm.js";
 import NewListing from "./components/NewListing.js";
 
 function App() {
+  const url = "http://localhost:3000/product/";
+  const [listings, setListings] = useState([]);
+  useEffect(() => {
+    fetch(url, {
+      mode: "no-cors",
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data.data))
+      .catch((err) => console.log("oops, something went wrong", err));
+  });
+
   return (
     <div className="App">
       <HomePage />
