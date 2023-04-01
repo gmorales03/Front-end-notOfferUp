0b850122f846937f250adc54a26c9b4feedd961f
joedonovan <joepeterdonovan@gmail.com>
2023-04-01 13:02:01 -0500

    updated navbar to work with backend data

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function NavBar() {
+  const url = "http://localhost:3000/category/";
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setCategory(data);
+      })
+      .catch((err) => console.log("check your code, error", err));
+  }, []);
+  const cat = category.map(({title}) => {
+    return(
+      <ul>{title}</ul>
+    )
+  }) 
+
+  console.log(category);
   return (
     <div className="navBar">
-      <ul>
-        <li>category one</li>
-        <li>category two</li>
-        <li>category three</li>
-      </ul>
+      {cat}
     </div>
   );
 }
