3af9fa571798bcf874d6b22431e6972fbd19737a
amassock <ajmassock@gmail.com>
2023-04-01 10:54:52 -0500

    fix styling issues

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "./NavBar";
+import Box from "./Box.js";
 import Grid from "./Grid";
 import NewListing from "./NewListing";
 import "./homePage.css";
@@ -8,7 +9,7 @@ export default function HomePage(props) {
   const products = props.data.map(
     ({ _id, item, title, description, price, posted_at }) => {
       return (
-        <div className="product-container">
+        <div className="box">
           <p>{item}</p>
           <p>{price}</p>
           <p>{posted_at}</p>
@@ -20,7 +21,7 @@ export default function HomePage(props) {
   const databox = (
     <div className="container">
       <NavBar />
-      {products}
+      <div className="boxes">{products}</div>
     </div>
   );
   return <div className="result">{databox}</div>;
