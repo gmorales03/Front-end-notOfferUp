3a17ba70bd8ee58a2f7650422405979d9ea69a6f
joedonovan <joepeterdonovan@gmail.com>
2023-04-02 19:17:57 -0500

    listing page now renders with usestate data

--- src/components/categories/Appliances.js
+++ src/components/categories/Appliances.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import { useParams, useLocation } from 'react-router-dom'
 
-function Appliances() {
+function Appliances( props) {
+  let location =useLocation()
+  let params = useParams()
+  console.log(location.state.item); 
   return (
     <div className='applicances'>
       <h1>Appliances</h1>
